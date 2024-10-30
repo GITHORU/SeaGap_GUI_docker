350d9d092d396dd77b9d46a89a41bffb72a40ae4
GITHORU <hugo.reveneau@deess.eu>
2024-10-30 16:57:56 +0100

    Patching du script de build windows

--- build_exe.bat
+++ build_exe.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 call .\.venv\Scripts\activate.bat
 pyinstaller --noconfirm gui_windows.spec
-xcopy .\src\img .\dist\gui\img\ /s /e /h /y
+xcopy .\src\img .\dist\SeaGapDockerGUI\img\ /s /e /h /y
